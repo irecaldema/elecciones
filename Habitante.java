5625ccb559eb3028e2f20810675416c8b72b9dcc
zubiri <irecaldema14dw@ikzubirimanteo.com>
2014-11-05 12:03:04 +0000

    elecciones

--- Habitante.java
+++ Habitante.java
@@ -1,5 +1,13 @@
 import java.util.*;
 public class Habitante{
-    String nombre, apellido1, apellido2;
+    String nombre, apellido1, apellido2, direccion;
+    int edad;
+    public Habitante(String nombre,String apellido1,String apellido2,int edad ,String direccion){
+        this.nombre=nombre;
+        this.apellido1=apellido1;
+        this.apellido2=apellido2;
+        this.edad=edad;
+        this.direccion=direccion;
+    }
     
 }
